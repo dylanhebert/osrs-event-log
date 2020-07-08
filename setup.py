19cb178ea611173670764811099c0d8597796072
Harrison <harrisonhayes97@yahoo.com>
2020-07-07 22:33:56 -0500

    Add setup.py and remove requirements.txt.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='OSRS Discord Server Bot',
+      version='1.0',
+      description='OSRS Discord Server Bot',
+      author='Dylan Hebert',
+      install_requires=[
+        'discord>=1.0.1',
+        'bs4>=0.0.1',
+        ]
+     )
